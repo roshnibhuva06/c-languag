47cc9aab9e778323f54e3dcb9ae3121dc4c50f91
roshnibhuva06 <roshnibhuva6@gmail.com>
2024-06-24 11:47:02 +0530

    Add files via upload

---
+++ roshni.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	//data type var____name;//sytax
+	int x=10;
+	int y=20;
+	x=10;
+	y=20;
+	printf("x+y=%d\n",x+y);
+	printf("x-y=%d\n",x-y);
+	printf("x*y=%d\n",x*y);
+	printf("x/y=%d\n",x/y);
+	printf("x%y=%d\n",x%y);
+	
+	
+}
+
+
+
+	
+
+
