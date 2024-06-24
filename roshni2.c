b315d403b35a10172de768afbf191774f7f32e22
roshnibhuva06 <roshnibhuva6@gmail.com>
2024-06-24 11:47:21 +0530

    Add files via upload

---
+++ roshni2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("---------\n");
+	printf("|      |\n");
+	printf("|5*5=25|\n");
+	printf("|      |\n");
+	printf("---------\n");
+	
+}
+
+
+
+	
+
+
