a94ab86bf8d20f1ad66b16cf11deb08f14640dc2
roshnibhuva06 <roshnibhuva6@gmail.com>
2024-07-01 12:13:33 +0530

    Add files via upload

---
+++ name.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int x,y;
+    printf("enter number");
+    scanf("%d",&x);
+    peintf("enter number");
+    scanf("%d"&y);
+    (x,y)?printf("x is minimum"):printf("y is minimum");
+	
+}
