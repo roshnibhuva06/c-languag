e99b441714476d27bb94117cbc6342cf435488f4
roshnibhuva06 <roshnibhuva6@gmail.com>
2024-07-01 12:09:48 +0530

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int x,y;
+    printf("enter number");
+    scanf("%d",&x);
+    printf("enter number");
+    scanf("%d",&y);
+    (3,8)?printf("3 is minimum"):printf("8 is minimum");
+    
+    int n,z;
+    printf("enter number");
+    scanf("%d",&n);
+    printf("enter number");
+    scanf("%d",&z);
+    (3,12)?printf("3 is minimum"):printf("12 is minimum");
+    
+	
+}
