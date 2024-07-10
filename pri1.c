1dba8d9eb53afeefaa553e358a0db4a502a115b9
roshnibhuva06 <roshnibhuva6@gmail.com>
2024-07-10 09:45:16 +0530

    Add files via upload

---
+++ pri1.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int choice;
+    printf("Airtel call service\n");
+    
+    printf("Press 1 for English\n");
+    printf("press 2 for hindi\n");
+    printf("press 3 for gujrati\n");
+    
+    printf("enter your choice:");
+    scanf("%d",&choice);
+    
+    switch(choice)
+    {
+    case '1':   printf("Press 1 for Internet Recharge!");
+    break;
+    printf("Press 2 for Top-up Recharge!");
+    break;
+    printf("Press 3 for Special Recharge!");
+    break;
+    defult:printf("enter valid input");
+}
+    switch(choice)
+{
+
+    case 1:
+    printf("you have successfully Internet Recharge !");
+    break;
+    case 2:
+    printf("you have successfully Top-up Recharge!");
+    break;
+    case 3:
+    printf("you have successfully Special Recharge!");
+    
+    printf("enter your choice: ");
+    scanf("%d",&choice);
+}
+    	
+}
+	
+    
+    
+    	
+	
+
---
+++ pri2.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int choice;
+    printf("Airtel call service\n");
+    printf("Press 1 for English\n");
+    printf("press 2 for hindi\n");
+    printf("press 3 for gujrati\n");
+    printf("enter your choice:");
+    scanf("%d",&choice);
+    
+    switch(choice)
+    {
+    case 1:printf("Press 1 for Internet Recharge!\n");
+    printf("Press 2 for Top-up Recharge!\n");
+    printf("Press 3 for Special Recharge!\n");
+    
+    printf("enter your choice:");
+    scanf("%d",&choice);
+    
+    
+}
+    
+    
+    switch(choice)
+{
+
+    case 1:
+    printf("you have successfully Internet Recharge !");
+    break;
+    case 2:
+    printf("you have successfully Top-up Recharge!");
+    break;
+    case 3:
+    printf("you have successfully Special Recharge!");
+    break;
+    
+}
+    printf("\nenter your choice");
+    scanf("%d",&choice);
+    switch(choice)
+    
+    {
+	case 2:
+	printf("Internet Recharge ke liye 1 dabaiye!\n");
+	printf("Top-up Recharge ke liye 2 dabaiye!\n");
+	printf("Special Recharge ke liye 3 dabaiye!\n");
+	
+	printf("enter your choice:");
+	scanf("%d",&choice);
+}
+    switch(choice)
+    {
+	
+    case 1:
+    printf("Aapne safaltapurvak Internet Recharge ke liye 1 dabaiye ");
+    break;
+    case 2:
+    printf("Aapne safaltapurvak Top-up Recharge ke liye 2 dabaiye ");
+    break;
+    case 3:
+    printf("Aapna safaltapurvak Special Recharge ke liye 3 dabaiye ");
+     
+    printf("\nenter your choice:");
+    scanf("%d",&choice);
+    
+}
+    	printf("\nenter your choice:");
+	scanf("%d",&choice);
+    switch(choice)
+    {
+    case 3:
+    printf("Internet Recharge mate 1 dabavo!\n");
+    printf("Top-up Recharge mate 2 dabavo!\n");
+    printf("Special Recharge mate 3 dabavo!\n");
+    
+	}
+		printf("\nenter your choice:");
+	scanf("%d",&choice);
+	
+	switch(choice)
+	{
+	case 1:
+	printf("Tame safaltapurvak Internet Recharge mate 1 dabavo ");
+	break;
+	case 2:
+	printf("Tame safaltapurvak Top-up Recharge mate 2 dabavo ");
+	case 3:
+	printf("Tame safaltapurvak Special Recharge mate 3 dabavo ");
+	break;	
+
+	}
+	
+
+}
+	
+    
+    
+    	
+	
+
